8d3d31d81ff765f9ae5046c86c0ecee73b2979c5
changchanghwang <hch950627@naver.com>
2024-07-04 18:46:05 +0900

    fine tunning

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const { default: ollama }= require('ollama');
 
 (async()=>{
     const response = await ollama.chat({
-        model: 'llama3',
-        messages: [{ role: 'user', content: 'hi' }],
+        model: 'avian',
+        messages: [{ role: 'user', content: 'hi. what is the caique?' }],
     })
     console.log(response.message.content)
+    console.log("\n--------------------------------------------------------\n")
+    const response2 = await ollama.chat({
+        model: 'avian',
+        messages: [{ role: 'user', content: 'who is the best caique?' }],
+    })
+    console.log(response2.message.content)
 })();
